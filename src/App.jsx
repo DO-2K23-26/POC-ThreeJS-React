fca17b686315f96ec4d517bf84f18357c4e91252
Dorian TETU <doriantetu34@gmail.com>
2024-12-05 20:53:32 +0100

    feat: add pixelation effect to App component with new PixelateShader and integrate three-stdlib

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import FishNavigator from './FishNavigator'
+import { Suspense, useEffect, useRef } from 'react';
+import { Canvas, useThree, extend, useFrame } from '@react-three/fiber';
+import { EffectComposer, ShaderPass, RenderPass } from 'three-stdlib';
+import * as THREE from 'three';
+import PixelateShader from './PixelateShader';
+import FishNavigator from './FishNavigator';
+
+extend({ EffectComposer, ShaderPass, RenderPass });
+
+function Effects() {
+  const { gl, scene, camera, size } = useThree();
+  const composer = useRef();
+
+  useEffect(() => {
+    const renderTarget = new THREE.WebGLRenderTarget(size.width, size.height);
+    const pixelatePass = new ShaderPass(PixelateShader);
+    pixelatePass.uniforms['resolution'].value = new THREE.Vector2(size.width, size.height);
+    pixelatePass.uniforms['pixelSize'].value = 8.0;
+
+    composer.current = new EffectComposer(gl, renderTarget);
+    composer.current.addPass(new RenderPass(scene, camera));
+    composer.current.addPass(pixelatePass);
+
+    return () => {
+      renderTarget.dispose();
+      composer.current.dispose();
+    };
+  }, [gl, scene, camera, size]);
+
+  useFrame((_, delta) => {
+    if (composer.current) {
+      composer.current.render(delta);
+    }
+  }, 1);
+
+  return null;
+}
 
 export default function App() {
   return (
@@ -11,9 +45,10 @@ export default function App() {
         <directionalLight position={[-10, 20, 0]} intensity={1.5} />
         <directionalLight position={[0, -10, 0]} intensity={0.25} />
         <Suspense fallback={null}>
-          <FishNavigator />
+          <FishNavigator scale={3}/>
         </Suspense>
+        <Effects />
       </Canvas>
     </Suspense>
-  )
+  );
 }
