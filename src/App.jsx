e81837d5f6eb583aca01e6164efabb48d5b94aac
Dorian TETU <doriantetu34@gmail.com>
2024-12-05 20:57:12 +0100

    feat: increase intensity of directional lights in App component for improved lighting

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,11 @@ export default function App() {
   return (
     <Suspense fallback={<span>loading...</span>}>
       <Canvas dpr={[1, 2]} camera={{ position: [-2, 2, 4], fov: 25 }}>
-        <directionalLight position={[10, 10, 0]} intensity={1.5} />
-        <directionalLight position={[-10, 10, 5]} intensity={1} />
-        <directionalLight position={[-10, 20, 0]} intensity={1.5} />
-        <directionalLight position={[0, -10, 0]} intensity={0.25} />
+        <directionalLight position={[10, 10, 0]} intensity={5} />
+        <directionalLight position={[-10, 10, 5]} intensity={5} />
+        <directionalLight position={[-10, 20, 0]} intensity={5} />
+        <directionalLight position={[0, -10, 0]} intensity={5} />
+        <directionalLight position={[0, 3, 0]} intensity={5} />
         <Suspense fallback={null}>
           <FishNavigator scale={3}/>
         </Suspense>
